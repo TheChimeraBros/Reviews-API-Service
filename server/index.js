385548d5d4884868bea98b386e6320d74e42f4aa
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-14 14:47:41 -0700

    set up server and router file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,41 @@
-require('express') from express;
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const router = express.Router();
+const reviews = require('./routes/reviews_route.js')
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use('/reviews', reviews);
+// one route /reviews which has query parameters
+    // '/' main query to get all reviews with parameters
+    // within /meta
+    // '/' for post
+    // to PUT requests with review id to update helpful or report
+      // '/review/report or helpful'
+
+const PORT = process.env.SERVER_PORT || 3000;
+app.listen(PORT);
+console.log(`SERVER LISTENING AT http://localhost:${PORT}`);
+
+
+
+/**
+ ** Create a main endpoint route that will take to router file
+ ** create file for meta data route and reviews route
+ *? what middlewares to use?
+ *! get in morgan middleware to log
+ ** Create  a function To log what kind of requests are coming in.
+ *! express.json , to parse incoming requests
+ *! express static to load html files , can be used when front end is imported
+ *
+ * TODO: import morgan , create morgan logger function
+ * TODO: import express.json
+ * TODO: import express.router
+ * * select port to listen to
+ *
+ *!TO use middleware you have use the app.use method
+ */
---
+++ server/routes/reviews_route.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.router()
+
+router.get('/', (req, res) => {
+  console.log('Got to 1st route');
+})
+/**
+ * * one route /reviews which has query parameters
+    // '/' main query to get all reviews with parameters
+    // within /meta
+    // '/' for post
+    // to PUT requests with review id to update helpful or report
+      // '/review/report or helpful'
+ */
+//
